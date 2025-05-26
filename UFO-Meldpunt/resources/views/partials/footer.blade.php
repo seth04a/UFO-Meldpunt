711cec6fb3a2da1e20a23eaed17672b9d46cc687
BtheBelgian <147116285+BtheBelgian@users.noreply.github.com>
2025-05-26 22:21:09 +0200

    Make basic header and footer

---
+++ UFO-Meldpunt/resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 bg-gray-100 p-4 shadow">
+     <h1 class="text-2xl font-bold text-gray-800">Made by: YouGotProbed</h1>
+</footer>
---
+++ UFO-Meldpunt/resources/views/partials/header.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Spot My Alien - @yield('title')</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+  <link rel="icon" href="{{asset('favicon.ico')}}">
+  </head>
+
+  <!-- Header Start -->
+   <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden bg-gray-100 p-4 shadow">
+        <h1 class="text-2xl font-bold text-gray-800">My Site Header</h1>
+            @if (Route::has('login'))
+                <nav class="flex items-center justify-end gap-4">
+                    @auth
+                        <a
+                            href="{{ url('/dashboard') }}"
+                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
+                        >
+                            Dashboard
+                        </a>
+                    @else
+                        <a
+                            href="{{ route('login') }}"
+                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
+                        >
+                            Log in
+                        </a>
+
+                        @if (Route::has('register'))
+                            <a
+                                href="{{ route('register') }}"
+                                class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                                Register
+                            </a>
+                        @endif
+                    @endauth
+                </nav>
+            @endif
+        </header>
