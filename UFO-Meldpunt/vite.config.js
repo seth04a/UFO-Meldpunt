daaa11b90faf78ff5d320651ab40f6edea626a64
Seth_Poelmans <seth.poelmans@gmail.com>
2025-05-27 10:13:48 +0200

    basic forms

--- UFO-Meldpunt/vite.config.js
+++ UFO-Meldpunt/vite.config.js
@@ -1,11 +1,19 @@
+<<<<<<< Updated upstream
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+=======
+import { defineConfig } from 'vite'
+import laravel, { refreshPaths } from 'laravel-vite-plugin'
+>>>>>>> Stashed changes
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: [
+                ...refreshPaths,
+                'app/Livewire/**',
+            ],
         }),
     ],
-});
+})
