94dd6cd8ff15d89bcaa3b0d9c89d18f9a3ad45bb
BtheBelgian <147116285+BtheBelgian@users.noreply.github.com>
2025-05-28 11:58:10 +0200

    Restores Post.php file

---
+++ UFO-Meldpunt/app/Models/Post.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Post extends Model
+{
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public $timestamps = false;
+}
